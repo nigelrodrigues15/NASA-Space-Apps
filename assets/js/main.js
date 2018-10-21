63f8edb2c2c193aebcb8bd339bd41423ede0f6fe
Nigel Rodrigues <nigel1@ualberta.ca>
2018-10-21 10:56:34 -0400

    Future

--- assets/js/main.js
+++ assets/js/main.js
@@ -398,4 +398,54 @@
 						$main._show(location.hash.substr(1), true);
 					});
 
+
+					
 })(jQuery);
+
+//MAP TOOLTIPS SCRIPT
+var tooltipNum = ['#nova-scotia', '#new-brunswick']
+
+$(tooltipNum).qtip({
+  content: function() {
+    return $( this ).data('tooltip'); //store data in data-tooltip
+    },
+  position: {
+    my: 'bottom center',  // Position my top left...
+    at: 'top center', // at the bottom right of...
+    adjust: {
+            y: 10
+        }
+  },
+  style: {
+    tip: {
+    corner: true,
+    corner: 'bottom center',
+    border: 1,
+    width: 15,
+    height: 7
+    }
+  }
+});
+
+var tooltipNum2 = ['#british-columbia', '#alberta', '#saskatchewan', '#manitoba', '#ontario', '#quebec']
+
+$(tooltipNum2).qtip({
+  content: function() {
+    return $( this ).data('tooltip'); //store data in data-tooltip
+    },
+  position: {
+    my: 'bottom center',  // Position my top left...
+    at: 'top center', // at the bottom right of...
+    adjust: {
+            y: 30
+        }
+  },
+  style: {
+    tip: {
+    corner: true,
+    border: 1,
+    width: 15,
+    height: 7
+    }
+  }
+});
